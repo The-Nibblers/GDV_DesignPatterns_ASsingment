ded3303b9862af8bf5f836bf20ad74c92fe003bd
Jens Ouwens <99173882+The-Nibblers@users.noreply.github.com>
2025-05-13 19:44:51 +0200

    Updated: Game manager with logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Player player;
+
+    private Enemy Enemy;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = new Player();
+        Enemy = new Enemy();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            player.Shoot(Enemy.enemyHealth);
+        }
     }
 }
